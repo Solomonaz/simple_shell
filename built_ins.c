5f36fd395d350e17cb9ad5cde4fa321bb50b0233
Yom-Gese <105068190+Yom-Gese@users.noreply.github.com>
2022-08-15 23:12:53 +0300

    Update built_ins.c

--- built_ins.c
+++ built_ins.c
@@ -1,95 +1,23 @@
 #include "main.h"
 
 /**
- * findBuiltIns - validates if command is builtin and executes
- * @build: input build
- * Return: true if found, false if not
+ * builtins - function that implements the exit and printenv builtins
+ * @line: the buffer
+ * @args: the parsed arguments
+ * @env: the environment
+ * @ex_st: the exit status
+ * Return: void
  */
-_Bool findBuiltIns(config *build)
+void builtins(char *line, char **args, char **env, int *ex_st)
 {
-	register int i = 0;
-	type_b getBuiltIns[] = {
-		{"exit", exitFunc},
-		{"env", envFunc},
-		{"history", historyFunc},
-		{"alias", aliasFunc},
-		{"cd", cdFunc},
-		{"setenv", setenvFunc},
-		{"unsetenv", unsetenvFunc},
-		{"help", helpFunc},
-		{NULL, NULL}
-	};
 
-	while (getBuiltIns[i].command)
+	if (_strcmp(args[0], "exit") == 0)
 	{
-		if (_strcmp(build->args[0], getBuiltIns[i].command) == 0)
-		{
-			getBuiltIns[i].func(build);
-			freeArgsAndBuffer(build);
-			return (true);
-		}
-		i++;
+		free(args);
+		free(line);
+		exit(*ex_st);
 	}
-	return (false);
-}
 
-/**
- * exitFunc - exits the application
- * @build: input build
- * Return: 1 on success, 0 on failure
- */
-int exitFunc(config *build)
-{
-	register int argCount, exitStatus;
-
-	argCount = countArgs(build->args);
-	if (argCount == 1)
-	{
-		freeMembers(build);
-		if (build->errorStatus)
-			exit(build->errorStatus);
-		exit(EXIT_SUCCESS);
-	}
-	else if (argCount > 1)
-	{
-		exitStatus = _atoi(build->args[1]);
-		if (exitStatus == -1)
-		{
-			errno = EILLEGAL;
-			build->errorStatus = 2;
-			errorHandler(build);
-			return (0);
-		}
-		freeMembers(build);
-		exit(exitStatus);
-	}
-	return (1);
-}
-
-/**
- * historyFunc - displays command history
- * @build: input build
- * Return: 1 on success, 0 on failure
- */
-int historyFunc(config *build)
-{
-	char *str = "Currently in development\n";
-
-	(void)build;
-	write(STDOUT_FILENO, str, _strlen(str));
-	return (1);
-}
-
-/**
- * aliasFunc - displays local aliases
- * @build: input build
- * Return: 1 on success, 0 on failure
- */
-int aliasFunc(config *build)
-{
-	char *str = "Currently in development\n";
-
-	(void)build;
-	write(STDOUT_FILENO, str, _strlen(str));
-	return (1);
+	if (_strcmp(args[0], "env") == 0)
+		printenv(env, ex_st);
 }
